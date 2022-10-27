07ce5f1e784e3f05520d282f193d32b9bae28995
Prince Oncada <prince.oncada@gmail.com>
2022-10-28 07:23:52 +0800

    Adding previous assignments to repository

---
+++ Home Assignment. Variables & Data Types/saving-app.js
@@ -0,0 +1,5 @@
+let savingsGoal = 10000;
+let savingsCurrent = 7500;
+let percentAway = (1 - savingsCurrent/savingsGoal) * 100 
+
+console.log("Thank you for your discipline and hardwork, Sam! Your are now " + percentAway + "% away from your goal of saving P" + savingsGoal)
