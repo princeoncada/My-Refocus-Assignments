72387f62f5b5c0be47a0c3fd053561202ec4866e
Prince Oncada <prince.oncada@gmail.com>
2022-10-28 07:47:09 +0800

    Removing files

--- Home Assignment. Operators & Functions/tip-calculator.js
+++
@@ -1,5 +0,0 @@
-function computeTip(totalBill) {
-    return totalBill * 0.10
-}
-
-console.log("Your meal costs $100. Please leave $" + computeTip(100) + " tip.")
--- Home Assignment. Variables & Data Types/calorie-tracking-app.js
+++
@@ -1,8 +0,0 @@
-let minutesCycling = 30;
-let days = 15;
-let burntCalories = 225;
-
-console.log("Great work, Sam! After " + minutesCycling +
-            " minutes of cycling every day for " + days +
-            " days, you may lose a total of " + burntCalories 
-            * days + " calories.")
