384159dbf0c7e690bb8ad142e326d7ae5030b73c
Prince Oncada <prince.oncada@gmail.com>
2022-10-28 08:04:41 +0800

    Created BMI Calculator

---
+++ bmi_calculator.js
@@ -0,0 +1,6 @@
+let myWeight = 69
+let myHeight = 1.68
+let myBMI = myWeight/(Math.pow(myHeight, 2))
+
+console.log(`Prince's weight is ${myWeight} kg \nPrince's height is ${myHeight} m`)
+console.log(`Using these values, his BMI is ${myBMI.toFixed(2)}`)
